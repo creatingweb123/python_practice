db576a9cba7a7d7cacc5087dc91e12b08376b608
creatingweb123 <71700107+creatingweb123@users.noreply.github.com>
2023-09-12 10:58:18 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,70 @@
+import requests
+from datetime import datetime
+import time
 import smtplib
-import datetime as dt
-import pandas as pd
-import random
-##################### Extra Hard Starting Project ######################
 
-
-
-# should be between 1-3
-def make_letter_path():
-    letter_number = random.randint(1,3)
-    letter_path = f'/Users/user/OneDrive/바탕 화면/language/python 학습자료/birthday-wisher-extrahard-start/letter_templates/letter_{letter_number}.txt'
-    return letter_path
-
-def send_birthday_email(maden_letter,send_email):
-    my_email = 'shegegegey1@gmail.com'
-    my_text = f"Subject:Quote\n\n{maden_letter}"
+def send_email():
+    my_email = '-'
+    my_text = f"Subject:Quote\n\nThis is time to look at the sky for ISS"
 
     with smtplib.SMTP("smtp.gmail.com",port = 587) as connection:
         connection.starttls() # make safe connection
 
-        connection.login(my_email,'moeujpfulglyfajq')
+        connection.login(my_email,'-')
 
-        connection.sendmail(from_addr=my_email, to_addrs=send_email,msg=my_text)
+        connection.sendmail(from_addr=my_email, to_addrs='-',msg=my_text)
+
+MY_LAT = 37.541093
+MY_LONG = 126.980588
+
+parameter = {
+    "lat":MY_LAT,
+    "lng":MY_LONG,
+    "formatted":0
+}
 
 
-data = pd.read_csv("/Users/user/OneDrive/바탕 화면/language/python 학습자료/birthday-wisher-extrahard-start/birthdays.csv")
 
-birthday_data_dict = {(data_row["month"],data_row["day"]): data_row for (index,data_row) in data.iterrows()}
+def is_on_up():
+    response_ISS = requests.get(url="http://api.open-notify.org/iss-now.json")
+    # 모든 상황을 조건문으로 만들 필요없이 에러 발생시 해당 이유를 출력해주는 함수 
+    response_ISS.raise_for_status() 
+    longitude = float(response_ISS.json()["iss_position"]["longitude"])
+    latitude = float(response_ISS.json()["iss_position"]["latitude"])
+    if abs(longitude - MY_LONG) >5 or abs(latitude - MY_LAT) >5: return False
+    return True
+    
+def is_dark():    
+    today = datetime.now()
+    time_now = today.hour
+    response = requests.get(url="https://api.sunrise-sunset.org/json",params=parameter)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split('T')[1].split(':')[0])
+    sunrset = int(data["results"]["sunrset"].split('T')[1].split(':')[0])
+    if time_now >= sunrset or time_now <= sunrise:
+        return True
+    return False
+   
+operate = True
+while operate:
+    time.sleep(60)
+    if is_on_up() and is_dark():
+        send_email()
+        operate  = False
+        
 
-now = dt.datetime.now()
-today_tuple = (now.month, now.day)
+
+# # 1XX : Hold on         가다려라
+# # 2XX : Here You GO     완료
+# # 3XX : Go Away         권한이 없으니 가라
+# # 4XX : You Screwed Up  여러분이 잘못되었다
+# # 5XX : I Screwed Up    내가(서버가) 잘못되었다
 
 
-if today_tuple in birthday_data_dict:
-    letter_path = make_letter_path()
-    birthday_person = birthday_data_dict[today_tuple]
-    name = birthday_person['name']
-    email = birthday_person['email']
-    with open(letter_path, 'r') as file:
-            #
-            contents = file.read()
-            contents = contents.replace("[NAME]",name)
-    send_birthday_email(contents,email)
 
+
+
+
+# 1. if the iss is close to my_current_position 
+# and it is currently dark, send me an email to tell me to look up.
